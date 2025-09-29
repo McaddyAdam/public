7caa0a7ab02327f6feb17b4cddad9c6a50228306
Adam Adebesin <ade22001@byui.edu>
2025-09-29 22:30:59 +0100

    chore(build): add fallback frontend, make webpack tolerant, pin devDeps; implement Drive REST JSON and frontend handlers; add public composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpmudev/public-stub',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e722c3a8ce960ca148af2598190f4d1b56237a57',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wpmudev/public-stub' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e722c3a8ce960ca148af2598190f4d1b56237a57',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
